69c95e528de7e260584895055f530bd37fbee34a
arvaer <mjalmeida172@gmail.com>
2024-03-29 20:44:02 -0400

    update

--- src/dev_db.rs
+++
@@ -1,6 +0,0 @@
-use sqlx::{Pool, Postgres};
-
-type Db = Pool<Postgres>;
-
-const PG_DEV_POSTGRES_URL: &str = "postgres://postgress::welcome@localhost/postgres";
-const PG_DEV_APP_URL: &str = "postgres://postgress::welcome@localhost/app";
